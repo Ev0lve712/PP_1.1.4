a3ab448483f5d3098cee60b123b18dd709e6d2b3
Ev0lve <munlayt712@gmail.com>
2025-03-03 11:03:53 +0500

    добавление rollback(); + удаление лишних try

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.util.Util;
 import jm.task.core.jdbc.util.cmdSQL;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,36 +17,26 @@ public class UserDaoHibernateImpl implements UserDao, cmdSQL {
 
     @Override
     public void createUsersTable() {
+        Transaction tx = null;
         try (Session session = Util.getSession();) {
-            Transaction tx = session.beginTransaction();
+            tx = session.beginTransaction();
 
             session.createSQLQuery(cmdSQL.CREATE_TABLES).executeUpdate();
-            try {
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            }
-
+            tx.commit();
         } catch (Exception e) {
-
+            tx.rollback();
             e.printStackTrace();
         }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction tx = null;
         try (Session session = Util.getSession();) {
-            Transaction tx = session.beginTransaction();
+            tx = session.beginTransaction();
 
             session.createSQLQuery(cmdSQL.DROP_TABLES).executeUpdate();
-            try {
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            }
-
+            tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -53,22 +44,17 @@ public class UserDaoHibernateImpl implements UserDao, cmdSQL {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction tx = null;
         try (Session session = Util.getSession();) {
-            Transaction tx = session.beginTransaction();
+            tx = session.beginTransaction();
 
             User user = new User();
             user.setName(name);
             user.setLastName(lastName);
             user.setAge(age);
-
             session.save(user);
+            tx.commit();
 
-            try {
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -76,24 +62,19 @@ public class UserDaoHibernateImpl implements UserDao, cmdSQL {
 
     @Override
     public void removeUserById(long id) {
+        Transaction tx = null;
         try (Session session = Util.getSession();) {
-            Transaction tx = session.beginTransaction();
+            tx = session.beginTransaction();
 
             User user = session.get(User.class, id);
-
             if (user != null) {
                 session.delete(user);
             } else {
                 System.out.println("User not found");
             }
-            try {
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            }
-
+            tx.commit();
         } catch (Exception e) {
+            tx.rollback();
             e.printStackTrace();
         }
     }
@@ -106,11 +87,9 @@ public class UserDaoHibernateImpl implements UserDao, cmdSQL {
             session.beginTransaction();
 
             users = session.createQuery("FROM User", User.class).getResultList();
-
             for (User user : users) {
                 System.out.println(user);
             }
-
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -120,19 +99,14 @@ public class UserDaoHibernateImpl implements UserDao, cmdSQL {
 
     @Override
     public void cleanUsersTable() {
+        Transaction tx = null;
         try (Session session = Util.getSession();) {
-            Transaction tx = session.beginTransaction();
+            tx = session.beginTransaction();
 
             session.createSQLQuery(cmdSQL.CLEAN_TABLES).executeUpdate();
-
-            try {
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            }
-
+            tx.commit();
         } catch (Exception e) {
+            tx.rollback();
             e.printStackTrace();
         }
     }
